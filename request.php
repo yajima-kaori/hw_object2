fe691c73d1feeffdcab0c8545a4496180bd01051
yajimakaori <kaori.yajima19891029@gmail.com>
2016-06-11 17:49:26 +0000

    first commit

--- request.php
+++ request.php
@@ -3,34 +3,34 @@
 require_once('RequestController.php');
 
 // URL最後の数字を取得する
-$param = substr(strstr($_SERVER["REQUEST_URI"], '?'),-1);
+$param = $_GET['action'];
 // var_dump($param);
 
 $requestController = new RequestController();
 
-$action =  $requestController->header();
-//エラーになる
-$action =  $requestController->action . $param . ();
+if(method_exists($requestController,$param))
+{
+  $action =  $requestController->header();
+  $action =  $requestController->$param();
+  $action =  $requestController->footer();
+  $action =  $requestController->getResponse();
 
-
-//
-if()
+  echo $action[0] . "<br>";
+  echo $action[1] . "<br>";
+  echo $action[2];
+}
+else
 {
   $action =  $requestController->pageNotFound();
-  echo $action[0] ;
+  $action =  $requestController->getResponse();
+  echo $action[0];
 }
 
-$action =  $requestController->footer();
-$action =  $requestController->getResponse();
-
-// var_dump($action);
-
-echo $action[0] . "<br>";
-echo $action[1] . "<br>";
-echo $action[2];
 
 
-//めりっとかく
+
+//めりっと
+// request.php を変更する必要がない
 
 
 
